d2f29d51665353e331600e0a14b3a6a558cd6ea1
Your Name <rahulchintu.b@gmail.com>
2025-03-18 08:06:17 +0530

    Expense app

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,16 @@
 #!/bin/bash
-Component=frontend
-logfile = /home/ec2-user/Expense-Shell/frontend.log
+$Component=frontend
+logfile =temp/$Component.log
 
 stat() {
 if [ ! $? -eq 0 ]; then
-    echo "Error in installing nginx" & >> $logfile
+    echo "Error in installing nginx" & >> logfile
     exit 1
     fi
 }
 
-echo "Installing nginx application" >> $logfile
+echo "Installing nginx application" >> logfile
 dnf install nginx -y
 stat $?
 
-systemctl enable nginx >> $logfile
-stat $?
 
-rm -rf /usr/share/nginx/html/* >> $logfile
-stat $?
-
-curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip >> $logfile
-stat() 
-
-cd /usr/share/nginx/html >> $logfile 
-stat()
-
-unzip /tmp/frontend.zip >> $logfile
-stat()
-
-systemctl restart nginx >> $logfile
-stat()
