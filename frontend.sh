102e8ec38d181ee84b8212299b354836f6a11aa7
Your Name <rahulchintu.b@gmail.com>
2025-03-18 08:40:00 +0530

    Expense app

--- frontend.sh
+++ frontend.sh
@@ -13,11 +13,19 @@ echo -e "Installing nginx application"
 dnf install nginx -y &>> $logFile
 Stat $?
 
+echo -n "Clearning Old $component Content:"
+rm -rf /usr/share/nginx/html/* 
+Stat $?
+
+echo -e "Downloading frontend content"
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip &>> $logFile
+Stat $?
+
 echo -e "Enabling nginx application"
-systemctl enable nginx   &>> $logFile
+systemctl enable nginx &>> $logFile
 
 echo -e "Restarting Nginx successfully"
-systemctl restart nginx  &>> $logFile
+systemctl restart nginx &>> $logFile
 
 
 
