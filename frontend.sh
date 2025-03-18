be1cd3615fc54f023dbaa412b936ccb0577e92e6
Your Name <rahulchintu.b@gmail.com>
2025-03-18 08:11:34 +0530

    Expense app

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-$Component=frontend
-logfile =temp/$Component.log
 
-stat() {
-if [ ! $? -eq 0 ]; then
-    echo "Error in installing nginx" & >> logfile
-    exit 1
-    fi
-}
-
-echo "Installing nginx application" >> logfile
+echo "Installing nginx application"
 dnf install nginx -y
-stat $?
+
+systemctl enable nginx   
+systemctl restart nginx
 
 
