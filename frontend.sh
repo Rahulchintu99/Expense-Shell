e903900fd01461a4122b9ca55ca86f355a54a640
Your Name <rahulchintu.b@gmail.com>
2025-03-18 08:23:15 +0530

    Expense app

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "Installing nginx application"
-dnf install nginx -y &>> /tmp/frontend.log
+echo -e "Installing nginx application"
 
+dnf install nginx -y &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+  echo -e "Nginx application has been installed successfully"
+else
+  echo -e "Nginx application installation failed"
+  exit 1
+fi
+
+echo -e "Enabling nginx application" &>> /tmp/frontend.log
 systemctl enable nginx   &>> /tmp/frontend.log
+
+echo -e "Restarting Nginx successfully" &>> /tmp/frontend.log
 systemctl restart nginx  &>> /tmp/frontend.log
+
 
 
